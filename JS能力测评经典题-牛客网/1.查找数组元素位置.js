c189aec069aa9c79f5bef424eeae96cb6bb81e75
DangoSky <1341514988@qq.com>
2018-11-18 14:39:54 +0800

    coding!

--- JS能力测评经典题-牛客网/1.查找数组元素位置.js
+++ JS能力测评经典题-牛客网/1.查找数组元素位置.js
@@ -1,3 +1,18 @@
+// 头脑简单
 function indexOf(arr, item) {
     return arr.indexOf(item);
+}
+// 没想到的兼容性写法
+function indexOf(arr, item) {
+	if (Array.prototype.indexOf){   //判断当前浏览器是否支持
+		return arr.indexOf(item);
+	} 
+	else {
+		for (var i = 0; i < arr.length; i++){
+			if (arr[i] === item){
+				return i;
+		    }
+	    }
+	}     
+	return -1;     
 }
