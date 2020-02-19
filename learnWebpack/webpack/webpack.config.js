cf35b19df496c119f384a3c9cb244eed25ae5b62
dangosky <1341514988@qq.com>
2020-02-19 18:39:51 +0800

    feat: learn css bale
    Change-Id: I5c4d823b3a27642bd1773c7405fe10eeaf0af26e

--- learnWebpack/webpack/webpack.config.js
+++ learnWebpack/webpack/webpack.config.js
@@ -18,23 +18,31 @@ module.exports = {
     new ExtractTextPlugin("css/style.css"),
   ],
   module: {
-    rules: [
-      // {
-      //   test: /\.css$/,
-      //   use: ['style-loader', 'css-loader'] // 从右向左解析原则。执行时先用 css-loader 将 css 文件转成对象，传递给 style-loader 插入到 head 的 style 中。这样是将css打包到对应的js里面去，所以页面加载的时候会先解析完了js之后再去加载css，会影响用户体验
-      // },
-      // {
-      //   test: /\.less$/,
-      //   use: ['style-loader', 'css-loader', 'less-loader'] // 从右向左解析原则。先解析成css
-      // },
-      {
-        test: /\.css|.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })      
-      },
-
-    ]
+    rules: [{
+      test: /\.css|.less$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader', { 
+          loader: 'postcss-loader', 
+            options: {
+              plugins: [
+                require('autoprefixer') ({
+                  // 要适配的浏览器
+                  overrideBrowserslist: [
+                    "defaults",
+                    "not ie < 11",
+                    "last 2 versions",
+                    "> 1%",
+                    "iOS 7",
+                    "last 3 iOS versions"
+                  ]
+                })
+              ]
+            }
+          },
+          'less-loader'
+        ]
+      })      
+    }]
   }
 }
