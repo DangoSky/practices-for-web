6095f901c4c1ab56caffe94b66e1079e60c41978
dangosky <1341514988@qq.com>
2020-02-20 19:51:20 +0800

    feat: learn bale pictures
    Change-Id: I052bba2dfcce60355aebafbc3e1023b17693a214

--- learnWebpack/webpack/webpack.config.js
+++ learnWebpack/webpack/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: path.resolve(__dirname,'../src/main.js'),    // 入口文件
   output: { // 出口
     filename: '[name].[hash:8].js',      // 打包后的文件名称
-    path: path.resolve(__dirname,'../dist')  // 打包后的目录
+    path: path.resolve(__dirname,'../dist'),  // 打包后的目录
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -18,31 +18,64 @@ module.exports = {
     new ExtractTextPlugin("css/style.css"),
   ],
   module: {
-    rules: [{
-      test: /\.css|.less$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', { 
-          loader: 'postcss-loader', 
-            options: {
-              plugins: [
-                require('autoprefixer') ({
-                  // 要适配的浏览器
-                  overrideBrowserslist: [
-                    "defaults",
-                    "not ie < 11",
-                    "last 2 versions",
-                    "> 1%",
-                    "iOS 7",
-                    "last 3 iOS versions"
-                  ]
-                })
-              ]
-            }
-          },
-          'less-loader'
-        ]
-      })      
-    }]
+    rules: [
+      {
+        test: /\.css|.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', { 
+            loader: 'postcss-loader', 
+              options: {
+                plugins: [
+                  require('autoprefixer') ({
+                    // 要适配的浏览器
+                    overrideBrowserslist: [
+                      "defaults",
+                      "not ie < 11",
+                      "last 2 versions",
+                      "> 1%",
+                      "iOS 7",
+                      "last 3 iOS versions"
+                    ]
+                  })
+                ]
+              }
+            },
+            'less-loader'
+          ]
+        })      
+      }, 
+      // {
+      //   test: /\.(jpeg|jpg|png|gif)$/,
+      //   loader: 'file-loader',
+      //   options: {
+      //     name: '[name].[hash:8].[ext]',
+      //     publicPath: '../images/',
+      //     outputPath: 'images/'
+      //   }
+      // },
+      {
+        test: /\.(jpeg|jpg|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 200000,
+          name: '[name].[hash:8].[ext]',
+          publicPath: '../images/',
+          outputPath: 'images/'
+        }
+      },
+
+  
+
+      // {
+      //   test: /\.html$/,
+      //   use: [{
+      //     loader: 'html-loader',
+      //     options: {
+      //       // minimize: true // 是否压缩打包后的 HTML 文件，true 的话则 HTML 文件会丢失空格和换行
+      //     }
+      //   }],
+      // }
+    ]
   }
 }
